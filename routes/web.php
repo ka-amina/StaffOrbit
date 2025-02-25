ff89c1a16e33a126f722525a7187779eaca31669
ka-amina <aminakara400@gmail.com>
2025-02-25 20:30:00 +0000

    SO-6: ajouter le route pour la page de  departements

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\Departments;
 
 Route::view('/', 'welcome');
 
@@ -12,4 +13,15 @@ Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
-require __DIR__.'/auth.php';
+Route::get('/departments', Departments::class)->name('departments');
+// Route::get('/departments', function () {
+//     return view('departments');
+// })->name('departments');
+
+// Route::middleware(['auth', 'check.permission:view departments'])->group(function () {
+//     Route::get('/departments', function () {
+//         return view('departments');
+//     })->name('departments');
+// });
+
+require __DIR__ . '/auth.php';
